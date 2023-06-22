b3fe4277afc551aa6cd3abf2dac406229534ed60
Nanahandresaputra <nanaggmu@gmail.com>
2023-06-22 19:45:27 +0700

    edit header

--- src/components/home/pricing/card-pricing/cardPricing.jsx
+++ src/components/home/pricing/card-pricing/cardPricing.jsx
@@ -1,12 +1,12 @@
 import { AiOutlineArrowRight } from "../../../../../node_modules/react-icons/ai";
 
-const CardPricing = ({ imgPricing, price }) => {
+const CardPricing = ({ imgPricing, price, type }) => {
   return (
     <section className="flex justify-center">
       <div className="bg-white py-5 px-8 shadow-lg h-64 w-58  rounded-2xl shadow-blue-500 sticky lg:h-80 lg:w-80">
         <img src={imgPricing} alt="bronze" className="w-10 mb-4 lg:w-auto lg:h-auto" />
         <h1 className="text-2xl font-semibold text-blue-500 lg:text-4xl">{price}</h1>
-        <h1 className="text-2xl font-medium font-clash-display lg:my-3 lg:text-4xl">HiBronze</h1>
+        <h1 className="text-2xl font-medium font-clash-display lg:my-3 lg:text-4xl">{type}</h1>
         <p className="text-justify text-sm w-44 border-t pt-2 mt-3 lg:w-52 lg:text-base">There are many variations of passages of Lorem Ipsum available, </p>
       </div>
       <div className="bg-blue-950 shadow-md p-2 w-58 h-66 mt-7 rounded-2xl -ms-52 flex items-end lg:mt-5 lg:-ms-72 lg:h-84 lg:w-80">
--- src/components/home/pricing/pricing.jsx
+++ src/components/home/pricing/pricing.jsx
@@ -10,9 +10,9 @@ const Pricing = () => {
           <button className="btn-blue-rounded-full my-3 font-medium md:h-14 md:text-lg lg:h-12 lg:w-64">Hubungi admin</button>
         </div>
         <div className="mt-10 flex flex-col justify-center space-y-9 md:mt-14 md:flex-row md:space-y-0 md:space-x-3 lg:mt-14 lg:justify-between">
-          <CardPricing imgPricing={hiBronze} price="IDR 2,200" />
-          <CardPricing imgPricing={hiSilver} price="IDR 3,500" />
-          <CardPricing imgPricing={hiGold} price="IDR 5,500" />
+          <CardPricing imgPricing={hiBronze} price="IDR 2,200" type="HiBronze" />
+          <CardPricing imgPricing={hiSilver} price="IDR 3,500" type="HiSilver" />
+          <CardPricing imgPricing={hiGold} price="IDR 5,500" type="HiGold" />
         </div>
       </div>
     </section>
